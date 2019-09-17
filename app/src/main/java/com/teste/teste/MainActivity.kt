14a7d33672c2db5e71a8e5614ae84be557f1a066
dev02@saeinfo <dev02@saeinfo.com.br>
2019-09-17 15:46:03 -0400

    Hello world to Teste

--- app/src/main/java/com/teste/teste/MainActivity.kt
+++ app/src/main/java/com/teste/teste/MainActivity.kt
@@ -2,11 +2,22 @@ package com.teste.teste
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val teste: TextView
+        teste = label
+
+        teste.setText("Teste")
+
+
     }
 }
